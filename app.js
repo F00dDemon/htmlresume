d623c7a3325a9741d1afee9d72290727d9d1c3fa
f00d <delossantos.orion@student.greenriver.edu>
2025-11-16 23:46:50 -0800

    added database

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mysql2 from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
+
+const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}).promise();
 
 app.set('view engine', 'ejs');
 
@@ -27,20 +39,55 @@ app.get('/confirm', (req, res) => {
     res.render('confirmation');
 });
 
-app.get('/admin', (req, res) => {
+app.get('/admin', async(req, res) => {
 
-    res.render('admin', { contacts });
+    try {
+        const [contacts] = await 
+        pool.query('SELECT * FROM contacts ORDER BY date_added desc');
+        res.render('admin', { contacts });
+    } catch(err) {
+        console.error('Database error:', err);
+    }
 });
 
-app.post('/submit-contact', (req, res) => {
+app.get('/db-test', async(req, res) => {
+    try {
+        const [contacts] = await pool.query('SELECT * FROM contacts');
+        res.send(contacts);
+    } catch(err) {
+        console.error('Database error:', err);
+    }
+});
+
+app.post('/submit-contact', async(req, res) => {
 
     const contact = req.body;
     contact.timestamp = new Date()
 
-    contacts.push(contact);
+    const sql = "insert into contacts (first_name, last_name, job, company, linkedin, email, how_we_met, other, date_added) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
     console.log(contacts);
 
-    res.render('confirmation', { contact });
+    const params = [
+        contact.fname,
+        contact.lname,
+        contact.jobname,
+        contact.companyinput,
+        contact.linkedinput,
+        contact.emailinput,
+        contact.howmeetinput,
+        contact.otherinput,
+        contact.timestamp
+    ]
+
+    try {
+        const [result] = await pool.execute(sql, params);
+        
+        // Send user to confirmation page
+        res.render('confirmation', { contact });
+    } catch(err) {
+        console.log("Database Error")
+    }
 });
 
 app.listen(PORT, () =>{
